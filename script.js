ac21dd2c89d21097f32da2daf24c1008bdf3d055
krishnakamalbaishnab <kamalkrishna98@gmail.com>
2024-08-19 00:19:16 +0100

    updtaed

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function toggleMenu() {
   const menu = document.querySelector(".menu-links"); //targeting the whole menu-links div
+  const icon = document.querySelector(".hamburger-icon");
+  menu.classList.toggle("open");
+  icon.classList.toggle("open");
 }
